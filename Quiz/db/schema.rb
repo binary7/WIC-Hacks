a0463d23cad06d9f8b71f7dcb502257198e6f961
Isabella <binary7@rocketmail.com>
2014-04-26 22:36:36 +0000

    fixed email in table

--- Quiz/db/schema.rb
+++ Quiz/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426164141) do
+ActiveRecord::Schema.define(version: 20140426223325) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140426164141) do
     t.string   "rec"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
--- Quiz/db/seeds.rb
+++ Quiz/db/seeds.rb
@@ -6,15 +6,15 @@ questions = Question.create([
   { body: 'I do not enjoy working on a team.'},
   { body: 'I am self-guided.'},
   { body: 'I would not be interested in things such as industrialization'},
-  { body: 'I do not like investigating problems by using experimentation and observation.'}
-  { body: 'I am not comfortable in a lab setting.'}
-  { body: 'I want to study with theoretical or abstract data.'}
-  { body: 'I prefer pushing the limits of computing power versus to solve problems with real-world applications.'}
-  { body: 'I enjoy programming computers to solve problems.'}
-  { body: 'I want to manipulate large quantities of data.'}
-  { body: 'I am interested in solving physical-world problems (ie. I would enjoy helping to design a building to survive earthquakes).'}
-  { body: 'I am okay if the problem I am working on has no direct real-world application.'}  
-  { body: 'I like looking for patterns and relationships.'}
+  { body: 'I do not like investigating problems by using experimentation and observation.'},
+  { body: 'I am not comfortable in a lab setting.'},
+  { body: 'I want to study with theoretical or abstract data.'},
+  { body: 'I prefer pushing the limits of computing power versus to solve problems with real-world applications.'},
+  { body: 'I enjoy programming computers to solve problems.'},
+  { body: 'I want to manipulate large quantities of data.'},
+  { body: 'I am interested in solving physical-world problems (ie. I would enjoy helping to design a building to survive earthquakes).'},
+  { body: 'I am okay if the problem I am working on has no direct real-world application.'},  
+  { body: 'I like looking for patterns and relationships.'},
   { body: 'Statistics and probability interest me.'}  
   ])
 
